83d4f0e5780b29a1a2f5893766713299fdf9ea93
VR314 <raghupathivaroon@gmail.com>
2022-05-31 13:01:02 +0000

    write G2 description

--- src/pages/G2.tsx
+++ src/pages/G2.tsx
@@ -5,10 +5,17 @@ const G2 = (props: any) => {
     <div style={{ justifyContent: "space-around", display: "flex" }}>
       <Agario />
       <div style={{ maxWidth: "50%" }}>
-        <h1>G2: Cell Growth</h1>
-        <p>The</p>
+        <h1>G2: Cell Growth (again) </h1>
+        <p>
+          The goal of G2 is to continue growth and finish final preparations for
+          cell division.
+        </p>
 
-        <p>Internally</p>
+        <p>
+          During the G2, the cell grows more, makes more proteins and
+          organelles, and begins to orient its organelles in preparation for
+          mitosis.
+        </p>
 
         <p>
           {"<"} The minigame to the left represents a cell consuming nutrients
